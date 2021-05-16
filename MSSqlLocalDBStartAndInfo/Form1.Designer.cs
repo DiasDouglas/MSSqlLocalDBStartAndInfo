972b7c5740376a6f44c293b93a05985e2f55856d
Douglas Dias <douglas.dias2102@outlook.com>
2021-05-16 01:38:52 -0300

    Add project files.

---
+++ MSSqlLocalDBStartAndInfo/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace MSSqlLocalDBStartAndInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.commandTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(206, 253);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(108, 23);
+            this.btnInfo.TabIndex = 0;
+            this.btnInfo.Text = "LocalDB Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(431, 253);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(108, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "LocalDB Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(66, 36);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(631, 146);
+            this.outputTextBox.TabIndex = 2;
+            this.outputTextBox.Text = "";
+            // 
+            // commandTextBox
+            // 
+            this.commandTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.commandTextBox.Location = new System.Drawing.Point(66, 197);
+            this.commandTextBox.Name = "commandTextBox";
+            this.commandTextBox.Size = new System.Drawing.Size(631, 30);
+            this.commandTextBox.TabIndex = 3;
+            this.commandTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(775, 303);
+            this.Controls.Add(this.commandTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnInfo);
+            this.Name = "Form1";
+            this.Text = "MSSQL LocalDB Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.RichTextBox commandTextBox;
+    }
+}
+
---
+++ MSSqlLocalDBStartAndInfo/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSSqlLocalDBStartAndInfo
+{
+    public partial class Form1 : Form
+    {
+        private Process Process { get; set; }
+        private string LocalDBFile 
+        { 
+            get
+            {
+                return "SqlLocalDB.exe";
+            }
+        }
+
+        private string LocalDBDefaultInfoArgument
+        {
+            get
+            {
+                return "i MSSQLLocalDB";
+            }
+        }
+
+        private string LocalDBDefaultStartArgument
+        {
+            get
+            {
+                return "s MSSQLLocalDB";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process = new Process();
+            Process.StartInfo.UseShellExecute = false;
+            Process.StartInfo.RedirectStandardOutput = true;
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            InitializeProcess(LocalDBDefaultInfoArgument);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            InitializeProcess(LocalDBDefaultStartArgument);
+        }
+
+        private void InitializeProcess(string arguments)
+        {
+            commandTextBox.Text = $"{LocalDBFile} {arguments}";
+
+            Process.StartInfo.FileName = LocalDBFile;
+
+            Process.StartInfo.Arguments = arguments;
+            Process.Start();
+            Process.WaitForExit();
+
+            string output = FormatOutput();
+
+            outputTextBox.CreateControl();
+            outputTextBox.Text = output;
+        }
+
+        private string FormatOutput()
+        {
+            string[] splittedOutput = Process.StandardOutput.ReadToEnd().Split('\n', '\r').Where(s => s.Length > 0).ToArray();
+            string output = "";
+
+            foreach (var line in splittedOutput)
+            {
+                output += $"{line}\n";
+            }
+
+            return output;
+        }
+    }
+}
